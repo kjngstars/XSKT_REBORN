6931525efdf8f0b307c3ed60e49eae1bace9827e
Nguyen Quoc Cuong <nqcuong0911@gmail.com>
2016-05-27 00:31:35 +0700

    some changes: chức năng xem danh sách đại lý

---
+++ PresentationLayer/FormDanhSachDaiLy.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogicLayer;
+
+namespace PresentationLayer
+{
+    public partial class FormDanhSachDaiLy : XtraForm
+    {
+        DanhSachDaiLyBUS agencyFiles = new DanhSachDaiLyBUS();
+        public FormDanhSachDaiLy()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDanhSachDaiLy_Load(object sender, EventArgs e)
+        {
+            var dtTable = agencyFiles.GetDanhSachDaiLy();
+            gcDanhSachDaiLy.DataSource = dtTable;
+            
+        }
+
+
+    }
+}
